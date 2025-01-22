6e16ab7b775e9b6f3d7de45a8954435b42c1ab7a
Krasnomir <micahlkrzywania7@gmail.com>
2025-01-22 21:54:51 +0100

    Added templates, some basic styling (using SCSS), and basic views handling

--- sharwicom/mainapp/views.py
+++ sharwicom/mainapp/views.py
@@ -2,9 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-# Create your views here.
 def index(request):
     template = loader.get_template('mainapp/main.html')
+    context = {}
+
+    return HttpResponse(template.render(context, request))
+
+def login(request):
+    template = loader.get_template('mainapp/login.html');
+    context = {}
+
+    return HttpResponse(template.render(context, request))
+
+def register(request):
+    template = loader.get_template('mainapp/register.html');
     context = {}
 
     return HttpResponse(template.render(context, request))
