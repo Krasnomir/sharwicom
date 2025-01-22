de46c2202d711ac24f6f20e4f36a719d7d5ed4b2
Krasnomir <micahlkrzywania7@gmail.com>
2025-01-22 22:27:41 +0100

    User authentication using login and register views

--- sharwicom/mainapp/views.py
+++ sharwicom/mainapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
+# TODO: Register data validation
 
 def index(request):
     template = loader.get_template('mainapp/main.html')
@@ -9,13 +13,44 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 def login(request):
-    template = loader.get_template('mainapp/login.html');
-    context = {}
+    if(request.method == "GET"):
+        template = loader.get_template('mainapp/login.html')
+        context = {}
 
-    return HttpResponse(template.render(context, request))
+        return HttpResponse(template.render(context, request))
+    if(request.method == "POST"):
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username=username,password=password)
+
+        if user is not None:
+            return HttpResponse("logged")
+        
+        template = loader.get_template('mainapp/login.html')
+        context = {}
+
+        return HttpResponse(template.render(context, request))
 
 def register(request):
-    template = loader.get_template('mainapp/register.html');
-    context = {}
+    if(request.method == "GET"):
+        template = loader.get_template('mainapp/register.html')
+        context = {}
 
-    return HttpResponse(template.render(context, request))
+        return HttpResponse(template.render(context, request))
+    
+    if(request.method == "POST"):
+        firstName = request.POST["first-name"]
+        lastName = request.POST["last-name"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+
+        user = User.objects.create_user(firstName, email, password)
+        user.last_name = lastName
+
+        user.save()
+
+        template = loader.get_template('mainapp/register.html')
+        context = {}
+
+        return HttpResponse(template.render(context, request))
+
