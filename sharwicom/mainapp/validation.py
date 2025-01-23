6592c1295f5923ad0f28571850710ad823720267
Krasnomir <micahlkrzywania7@gmail.com>
2025-01-23 09:25:17 +0100

    Register validation

---
+++ sharwicom/mainapp/validation.py
@@ -0,0 +1,35 @@
+import re
+from django.contrib.auth.models import User
+
+def validateRegister(username, first_name, last_name, email, password):
+
+    # check if username is already taken
+    if User.objects.filter(username=username).exists():
+        return "Username is already taken"
+
+    # validate username: 3-20 characters, only letters and numbers
+    userrname_pattern = r'^[a-zA-Z0-9]{3,20}$'
+    if not re.match(userrname_pattern, username):
+        return "Username must be 3-20 characters long and contain only letters and numbers."
+    
+    # validate first name: 2-50 characters, letters only
+    firstName_pattern = r'^[a-zA-Z]{2,50}$'
+    if not re.match(firstName_pattern, first_name):
+        return "First name must be 2-50 characters long and contain only letters."
+    
+    # validate last name: 2-50 characters, letters only
+    lastName_pattern = r'^[a-zA-Z]{2,50}$'
+    if not re.match(lastName_pattern, last_name):
+        return "Last name must be 2-50 characters long and contain only letters."
+    
+    # validate email: proper format
+    email_pattern = r'(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)])'
+    if not re.match(email_pattern, email, re.IGNORECASE):
+        return "Invalid email format."
+    
+    # validate password: 8+ characters, at least one special character and at least one number
+    password_pattern = r'^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&? "]).*$'
+    if not re.match(password_pattern, password):
+        return "Password must be at least 8 characters long, have at least one special character and at least one number"
+
+    return 0
--- sharwicom/mainapp/views.py
+++ sharwicom/mainapp/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.template import loader
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from .validation import validateRegister
 
 # TODO: Register data validation
 
@@ -24,12 +25,12 @@ def login(request):
         user = authenticate(username=username,password=password)
 
         if user is not None:
-            return HttpResponse("logged")
-        
-        template = loader.get_template('mainapp/login.html')
-        context = {}
+            return redirect('index')
+        else:
+            template = loader.get_template('mainapp/login.html')
+            context = {'error_message': "Incorrect username or password"}
 
-        return HttpResponse(template.render(context, request))
+            return HttpResponse(template.render(context, request))
 
 def register(request):
     if(request.method == "GET"):
@@ -39,18 +40,34 @@ def register(request):
         return HttpResponse(template.render(context, request))
     
     if(request.method == "POST"):
+        username = request.POST["username"]
         firstName = request.POST["first-name"]
         lastName = request.POST["last-name"]
         email = request.POST["email"]
         password = request.POST["password"]
 
-        user = User.objects.create_user(firstName, email, password)
-        user.last_name = lastName
+        validationMessage = validateRegister(username, firstName, lastName, email, password)
+        if(validationMessage) == 0:
+            user = User.objects.create_user(firstName, email, password)
+            user.last_name = lastName
+            user.username = username
 
-        user.save()
+            user.save()
 
-        template = loader.get_template('mainapp/register.html')
-        context = {}
+            template = loader.get_template('mainapp/register.html')
+            context = {}
 
-        return HttpResponse(template.render(context, request))
+            return HttpResponse(template.render(context, request))
+        else:
+            template = loader.get_template('mainapp/register.html')
+            context = {
+                'error_message': validationMessage,
+                'username': username,
+                'first_name': firstName,
+                'last_name': lastName,
+                'email': email,
+                'password': password,
+            }
+
+            return HttpResponse(template.render(context, request))
 
